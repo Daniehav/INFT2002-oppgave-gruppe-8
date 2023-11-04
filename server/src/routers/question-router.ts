ab72a7b667cac4cd244326330f87bdd2ce95ad34
Bigisak <yujinc727@gmail.com>
2023-11-04 14:01:06 +0100

    added  necessary files

--- server/src/routers/question-router.ts
+++ server/src/routers/question-router.ts
@@ -70,6 +70,17 @@ router.put('/:questionId', isAuthenticated, async (req, res) => {
     }
 });
 
+router.delete('/', isAuthenticated, (req, res) => {
+    questionService.getAllQuestions()
+        .then(questions => {
+            res.status(200).json(questions);
+        })
+        .catch(error => {
+            console.error('Failed to fetch questions:', error);
+            res.status(500).send('Internal Server Error');
+        });
+});
+
 router.get('/', isAuthenticated, (req, res) => {
     questionService.getAllQuestions()
         .then(questions => {
@@ -79,6 +90,26 @@ router.get('/', isAuthenticated, (req, res) => {
             console.error('Failed to fetch questions:', error);
             res.status(500).send('Internal Server Error');
         });
+});
+
+router.delete('/:questionId', isAuthenticated, async (req, res) => {
+    try {
+        const questionId = parseInt(req.params.questionId, 10);
+
+        if (isNaN(questionId)) {
+            return res.status(400).send('Invalid question ID');
+        }
+        const question = await questionService.getQuestionById(questionId); // check if question exist
+        await questionService.deleteQuestion(question.id, 1);
+        res.status(204).send();
+    } catch (error: unknown) {
+        if (error instanceof Error && error.message === 'No question found') {
+            res.status(404).send("Question not found");
+        }
+        else if (error instanceof Error) {
+            res.status(500).send('Internal Server Error');
+        }
+    }
 });
 
 function isAuthenticated(req: any, res: Response, next: NextFunction) {
